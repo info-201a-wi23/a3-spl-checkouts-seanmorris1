f9944062e5388049f8c6dfd7ed276e28f3bcc2da
seanmorris1 <98245418+seanmorris1@users.noreply.github.com>
2023-03-14 19:54:13 -0700

    Updated first chart

--- chart1_example.R
+++ chart1_example.R
@@ -1,6 +1,23 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
+library(dplyr)
+library(readr)
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+library(tidyverse)
+
+checkOutData <- read_csv("2017-2023-10-Checkouts-SPL-Data.csv")
+
+graphData <- dplyr::filter(checkOutData, MaterialType %in% c('MUSIC', 'EBOOK', 'MOVIE', 'AUDIOBOOK', 'SONG'))
+
+newdata <- dplyr::filter(checkOutData, MaterialType %in% c("MOVIE"))
+
+digitalAverages <- graphData %>%
+  group_by(MaterialType, CheckoutYear) %>%
+  summarize(Checkouts = mean(Checkouts))
+
+colnames(digitalAverages)[1]<-"Entertainment"
+
+digitalAverages %>%
+  ggplot(aes(CheckoutYear, Checkouts, colour = Entertainment))+
+  geom_point(size = 2, alpha = 1)+
+  geom_smooth(method="lm", se=FALSE, size=1)+
+  xlab("Year")+
+  labs(title = "Digital Entertainment Checkouts per Year")
